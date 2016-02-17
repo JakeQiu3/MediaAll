701e1c01fd1445b92e0842e6869b5e9718aa0dc1
qsyMac <qsy118614@163.com>
2016-02-17 23:38:52 +0800

    初始化视频截图

---
+++ Media All/AVFoundationCameraViewController.h
@@ -0,0 +1,13 @@
+//
+//  AVFoundationCameraViewController.h
+//  Media All
+//
+//  Created by qsyMac on 16/2/17.
+//  Copyright © 2016年 QSY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AVFoundationCameraViewController : UIViewController
+
+@end
---
+++ Media All/AVFoundationCameraViewController.m
@@ -0,0 +1,37 @@
+//
+//  AVFoundationCameraViewController.m
+//  Media All
+//
+//  Created by qsyMac on 16/2/17.
+//  Copyright © 2016年 QSY. All rights reserved.
+//
+
+#import "AVFoundationCameraViewController.h"
+
+@interface AVFoundationCameraViewController ()
+
+@end
+
+@implementation AVFoundationCameraViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
